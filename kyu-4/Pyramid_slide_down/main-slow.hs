940b832a078078889e647351b0aee0b62065a1a7
Abhishek <abhishekk19@gmail.com>
2019-09-25 20:50:35 +0530

    (4 kyu problem) Pyramid slide down (updated, haskell recursive version, slower, needs memoization Maybe).

---
+++ kyu-4/Pyramid_slide_down/main-slow.hs
@@ -0,0 +1,10 @@
+getHighestSum :: Int -> Int -> Int -> [[Int]] -> Int
+getHighestSum sum _ _ [] = sum
+getHighestSum sum minindex maxindex (x:xs) = max first second
+  where
+    first = getHighestSum (sum + (x !! minindex)) minindex (minindex + 1) xs
+    second = getHighestSum (sum + (x !! maxindex)) maxindex (maxindex + 1) xs
+
+longestSlideDown :: [[Int]] -> Int
+longestSlideDown (x:xs) = getHighestSum (head x) 0 1 xs
+
