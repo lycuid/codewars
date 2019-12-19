77f7271da8e7900207d8e4ba4511f439781ad2be
Abhishek S. Kadam <abhishekk19@gmail.com>
2019-12-19 15:40:06 +0530

    (5 kyu problem) Peano numbers.

--- kyu-5/Peano_numbers/main.hs
+++ kyu-5/Peano_numbers/main.hs
@@ -1,32 +1,34 @@
 -- https://www.codewars.com/kata/5779b0f0ec883247b2000117
+
 import Prelude hiding (even, odd, div, compare, Num, Int, Integer, Float, Double, Rational, Word)
 
 data Peano = Zero | Succ Peano deriving (Eq, Show)
 
 add, sub, mul, div :: Peano -> Peano -> Peano
 
+
 add Zero Zero = Zero
 add x Zero = x
 add Zero x = x
 add (Succ x) (Succ y) = Succ (Succ (add x y))
 
-
 sub Zero Zero = Zero
-sub (Succ x) Zero = Succ x
-sub Zero (Succ x) = error "negative number"
+sub x Zero = x
+sub Zero x = error "negative number"
 sub (Succ x) (Succ y) = sub x y
 
 mul Zero _ = Zero
 mul _ Zero = Zero
-mul (Succ x) (Succ Zero) = Succ x
-mul (Succ x) (Succ y) = add (Succ x) $ mul (Succ x) (Succ y `sub` Succ Zero)
+mul x (Succ Zero) = x
+mul x y = add x . mul x . sub y $ (Succ Zero)
 
 div _ Zero = error "divide by 0"
 div Zero _ = Zero
-div (Succ x) (Succ Zero) = Succ x
-div (Succ x) (Succ y)
-  | (Succ x) `compare` (Succ y) == LT = Zero
-  | otherwise = (Succ Zero) `add` (div (Succ x `sub` Succ y) (Succ y))
+div x (Succ Zero) = x
+div x y 
+  | compare x y == LT = Zero
+  | otherwise = add (Succ Zero) . div (sub x y) $ y
+
 
 
 even, odd :: Peano -> Bool
@@ -37,10 +39,9 @@ even (Succ (Succ x)) = even x
 
 odd = not . even
 
-
 compare :: Peano -> Peano -> Ordering
 compare Zero Zero = EQ
-compare (Succ x) Zero = GT
-compare Zero (Succ x) = LT
+compare x Zero = GT
+compare Zero x = LT
 compare (Succ x) (Succ y) = compare x y
 
