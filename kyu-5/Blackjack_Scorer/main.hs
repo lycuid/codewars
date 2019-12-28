d6c662d38695f07907bcf07267a00e2a8bdaf0cb
Abhishek S. Kadam <abhishekk19@gmail.com>
2019-12-28 18:16:35 +0530

    Blackjack Scorer (updated).

--- kyu-5/Blackjack_Scorer/main.hs
+++ kyu-5/Blackjack_Scorer/main.hs
@@ -9,15 +9,16 @@ blackjackScore x
   where
     isNumber = isDigit . head
 
-blackjackSafeScore :: [String] -> (Int, Int)
-blackjackSafeScore xs = (length aces, safe_score)
+blackjackScorePartition :: [String] -> (Int, Int)
+blackjackScorePartition xs = (length aces, safe_score)
   where
     (aces, rest) = partition ((==) "A") xs
     safe_score = (+) (length aces) . sum . map blackjackScore $ rest
 
+
 scoreHand :: [String] -> Int
 scoreHand xs = safe_score + required_score
   where
-    (ace_len, safe_score) = blackjackSafeScore xs
+    (ace_len, safe_score) = blackjackScorePartition xs
     required_score = (*) 10 . min ace_len . max 0 $ ((21 - safe_score) `div` 10)
 
