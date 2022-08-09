9f8159868fe93fb0e5943e3b279c9566520e2572
Abhishek Kadam <abhishekk19@gmail.com>
2022-08-09 16:21:19 +0530

    (4 kyu problem) Determining if a graph has a solution.

---
+++ kyu-4/Determining_if_a_graph_has_a_solution/main.hs
@@ -0,0 +1,10 @@
+-- https://www.codewars.com/kata/53223653a191940f2b000877/haskell
+
+type Node = Char
+type Arc = (Node, Node)
+
+solveGraph :: Node -> Node -> [Arc] -> Bool
+solveGraph s d arcs = s == d || any (\source -> solveGraph source d dests) srcs
+  where
+    srcs  = map snd . filter ((==) s . fst) $ arcs
+    dests = filter ((/=) s . snd) arcs
