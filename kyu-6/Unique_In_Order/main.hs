c65e88cbf65cb977fe4353e928a40636f49e8f82
Abhishek Kadam <abhishekk19@gmail.com>
2022-08-16 01:33:20 +0530

    (6 kyu problem) Unique in order (cleanup, refactor).

--- kyu-6/Unique_In_Order/main.hs
+++ kyu-6/Unique_In_Order/main.hs
@@ -1,5 +1,5 @@
 -- https://www.codewars.com/kata/54e6533c92449cc251001667/haskell
-import           Data.List (concat, group)
+import Data.List (group)
 
 uniqueInOrder :: Eq a => [a] -> [a]
 uniqueInOrder = map head . group
