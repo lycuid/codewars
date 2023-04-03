acf735323ce0882aaecfad772acabd5cfad08a17
Abhishek Kadam <abhishekk19@gmail.com>
2023-04-03 22:03:36 +0530

    (6 kyu problem) Tribonacci sequence.

---
+++ kyu-6/Tribonacci_Sequence/main.hs
@@ -0,0 +1,6 @@
+-- https://www.codewars.com/kata/556deca17c58da83c00002db/train/haskell
+
+tribonacci :: Num a => (a, a, a) -> Int -> [a]
+tribonacci (a, b, c) n = take n $ tribo (a, b, c)
+  where
+    tribo (a, b, c) = a : tribo (b, c, a + b + c)
